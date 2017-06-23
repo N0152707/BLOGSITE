fafd2a0884c2a1cd62a536fc94f96d219779d2e1
markbrandt <mark.brandt@libertymutual.com>
2017-06-23 13:57:24 -0400

    6/23/2017 mark - checking in blogdaostubimpl

--- BlogSite/src/main/java/com/sg/blogsite/dao/CategoryDaoDbStubImpl.java
+++ BlogSite/src/main/java/com/sg/blogsite/dao/CategoryDaoDbStubImpl.java
@@ -5,10 +5,54 @@
  */
 package com.sg.blogsite.dao;
 
+import com.sg.blogsite.model.Category;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author apprentice
  */
-public class CategoryDaoDbStubImpl {
+public class CategoryDaoDbStubImpl implements CategoryDao {
+
+    private Category onlyCategory;
+    private List<Category> categoryList = new ArrayList<>();
+
+    public CategoryDaoDbStubImpl() {
+        onlyCategory = new Category();
+        onlyCategory.setCategoryName("Category Scary");
+
+        categoryList.add(onlyCategory);
+
+    }
+
+    @Override
+    public Category createCategory(Category category) {
+        if (category.getCategoryName().equals(onlyCategory.getCategoryName())) {
+            return onlyCategory;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Category readCategory(int categoryId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateCategory(Category category) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeCategory(int categoryId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
