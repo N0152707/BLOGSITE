821d618d2a5e8b1e7e7a10902a5d497f6aad8420
eric.hebert <Eric.Hebert@LibertyMutual.com>
2017-06-23 10:19:08 -0400

    SQL and model  article typo fix 6-23-2017

---
+++ BlogSite/src/test/java/com/sg/blogsite/dao/BlogDaoTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.blogsite.dao;
+
+import com.sg.blogsite.model.Blog;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author apprentice
+ */
+public class BlogDaoTest {
+    
+    public BlogDaoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addBlog method, of class BlogDao.
+     */
+    @Test
+    public void testAddBlog() {
+    }
+
+    /**
+     * Test of updateBlog method, of class BlogDao.
+     */
+    @Test
+    public void testUpdateBlog() {
+    }
+
+    /**
+     * Test of getBlogById method, of class BlogDao.
+     */
+    @Test
+    public void testGetBlogById() {
+    }  
+    }
+    
+
---
+++ BlogSite/src/test/java/com/sg/blogsite/dao/CategoryDaoTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.blogsite.dao;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author apprentice
+ */
+public class CategoryDaoTest {
+    
+    public CategoryDaoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addCategory method, of class CategoryDao.
+     */
+    @Test
+    public void testAddCategory() {
+    }
+
+    /**
+     * Test of updateCategory method, of class CategoryDao.
+     */
+    @Test
+    public void testUpdateCategory() {
+    }
+
+    /**
+     * Test of getCategoryById method, of class CategoryDao.
+     */
+    @Test
+    public void testGetCategoryById() {
+    }
+
+    
+}
