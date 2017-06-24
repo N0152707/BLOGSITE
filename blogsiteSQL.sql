2fb5b639537e0ce263aaf1b7f5a2505ee6928da8
N0152707 <Eric.Hebert@Libertymutual.com>
2017-06-24 10:50:12 -0400

    updated sql

--- blogsiteSQL.sql
+++ blogsiteSQL.sql
@@ -4,14 +4,14 @@ CREATE SCHEMA IF NOT EXISTS blogsite;
 
 USE blogsite ;
 
-CREATE TABLE IF NOT EXISTS blogsite.category (
+CREATE TABLE IF NOT EXISTS category (
   category_id INT NOT NULL AUTO_INCREMENT,
   category_name VARCHAR(45) NOT NULL,
   PRIMARY KEY (category_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS blogsite.user_blog (
+CREATE TABLE IF NOT EXISTS user_blog (
   user_blog_id INT NOT NULL AUTO_INCREMENT,
   user_blog_name VARCHAR(45) NOT NULL,
   user_blog_password VARCHAR(45) NOT NULL,
@@ -21,72 +21,70 @@ CREATE TABLE IF NOT EXISTS blogsite.user_blog (
 );
 
 
-CREATE TABLE IF NOT EXISTS blogsite.blog (
+CREATE TABLE IF NOT EXISTS blog (
   blog_id INT NOT NULL AUTO_INCREMENT,
   blog_published VARCHAR(45) NOT NULL,
   blog_date_published DATE NOT NULL,
   blog_title VARCHAR(45) NOT NULL,
   blog_article VARCHAR(45) NOT NULL,
-  blog_deleted VARCHAR(45) NOT NULL,,
-  PRIMARY KEY (blog_id),
-);
+  blog_deleted VARCHAR(45) NOT NULL,
+  PRIMARY KEY (blog_id));
 
-CREATE TABLE IF NOT EXISTS blogsite.role (
+CREATE TABLE IF NOT EXISTS role (
   role_id INT NOT NULL,
   role_name VARCHAR(45) NOT NULL,
   PRIMARY KEY (role_id)
 );
 
-CREATE TABLE IF NOT EXISTS blogsite.tag (
+CREATE TABLE IF NOT EXISTS tag (
   tag_id INT NOT NULL AUTO_INCREMENT,
   tag_name VARCHAR(45) NOT NULL,
   PRIMARY KEY (tag_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS blogsite.blog_has_tag (
+CREATE TABLE IF NOT EXISTS blog_has_tag (
   blog_blog_id INT NOT NULL,
   tag_tag_id INT NOT NULL,
   PRIMARY KEY (blog_blog_id, tag_tag_id),
-  INDEX fk_blog_has_tag_tag1_idx (tag_tag_id ASC),
-  INDEX fk_blog_has_tag_blog1_idx (blog_blog_id ASC),
   CONSTRAINT fk_blog_has_tag_blog1
     FOREIGN KEY (blog_blog_id)
-    REFERENCES blogsite.blog (blog_id),
+    REFERENCES blog (blog_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT fk_blog_has_tag_tag1
     FOREIGN KEY (tag_tag_id)
-    REFERENCES blogsite.tag (tag_id)
+    REFERENCES tag (tag_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS blogsite.role_has_user_blog (
+CREATE TABLE IF NOT EXISTS role_has_user_blog (
   role_role_id INT NOT NULL,
   user_blog_user_blog_id INT NOT NULL,
   PRIMARY KEY (role_role_id, user_blog_user_blog_id),
   CONSTRAINT fk_role_has_user_blog_role1
     FOREIGN KEY (role_role_id)
-    REFERENCES blogsite.role (role_id),
+    REFERENCES role (role_id),
   CONSTRAINT fk_role_has_user_blog_user_blog1
     FOREIGN KEY (user_blog_user_blog_id)
-    REFERENCES blogsite.user_blog (user_blog_id)
+    REFERENCES user_blog (user_blog_id)
 );
 
-INSERT INTO `blogsite`.`role`
+INSERT INTO `role`
 (`role_id`,
 `role_name`)
 VALUES
 (1,"admin");
 
 
-INSERT INTO `blogsite`.`role`
+INSERT INTO `role`
 (`role_id`,
 `role_name`)
 VALUES
 (2,"write");
 
-
-INSERT INTO `blogsite`.`role`
+INSERT INTO `role`
 (`role_id`,
 `role_name`)
 VALUES
-(3,"read");
+(3,"read")
