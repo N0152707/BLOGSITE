97110b7b5dd24b5f32a79af6d4d93c95fdddbe62
markbrandt <mark.brandt@libertymutual.com>
2017-06-25 15:50:09 -0400

    6/25/2017 mark - checking in bloghastag

--- BlogSite/src/main/java/com/sg/blogsite/controller/BlogController.java
+++ BlogSite/src/main/java/com/sg/blogsite/controller/BlogController.java
@@ -6,10 +6,13 @@
 package com.sg.blogsite.controller;
 
 import com.sg.blogsite.model.Blog;
+import com.sg.blogsite.model.Category;
 import com.sg.blogsite.service.BlogServiceLayer;
 import com.sg.blogsite.service.CategoryServiceLayer;
+import java.time.LocalDate;
 import java.util.List;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +35,6 @@ public class BlogController {
     public String displayHome(Model model) {
         List<Blog> blogList = service.getLastFiveBlogs();
         model.addAttribute("blogList", blogList);
-<<<<<<< HEAD
         // Return the logical name of our View component
         return "blogSite";
     }
@@ -63,56 +65,23 @@ public class BlogController {
         String blogIdParameter = request.getParameter("blogId");
         int blogId = Integer.parseInt(blogIdParameter);
         Blog blog = service.readBlog(blogId);
-=======
->>>>>>> 15641ef0a4dd43963d629489d1171309a0c6498d
+
         model.addAttribute("blog", blog);
         return "index";
     }
-//
-//    //i don't think we need a addBlogForm in here, that is initially just displayed as an empty page to be filled in
-//    @RequestMapping(value = "/addBlog", method = RequestMethod.POST)
-//    public String addBlog(HttpServletRequest request) {
-//        Blog blog = new Blog();
-//        blog.setBlogPublished("N");
-//        blog.setBlogDatePublished(LocalDate.now());  //don't know how to parse a date from a string here
-//        blog.setBlogTitle("blogTitle");
-//        blog.setBlogArticle("blogArticle");
-//        blog.setBlogDeleted("N");
-//        service.createBlog(blog);
-//        return "redirect:displayBlogsPage";
-//    }
-//
-//    @RequestMapping(value = "/editBlogForm", method = RequestMethod.GET)
-//    public String displayEditBlogForm(HttpServletRequest request, Model model) {
-//        String blogIdParameter = request.getParameter("blogId");
-//        int blogId = Integer.parseInt(blogIdParameter);
-//        Blog blog = service.readBlog(blogId);
-//        model.addAttribute("blog", blog);
-//        return "editBlogForm";
-//    }
-//
-//    @RequestMapping(value = "/editBlog", method = RequestMethod.POST)
-//    public String editBlog(@Valid @ModelAttribute("blog") Blog blog,
-//            BindingResult result) {
-//        if (result.hasErrors()) {
-//            return "editBlogForm";
-//        }
-//        service.updateBlog(blog);
-//        return "redirect:displayBlogsPage";
-//    }
-//
-//    public String getAllBlogsByCategory(HttpServletRequest request, Model model) {
-//        String categoryIdParameter = request.getParameter("categoryId");
-//        int categoryId = Integer.parseInt(categoryIdParameter);
-//        List<Blog> blogList = service.getAllBlogsByCategory(categoryId);
-//        model.addAttribute("blogList", blogList);
-//        return "blog";
-//    }
-//
-//    @RequestMapping(value = "getAllCategories", method = RequestMethod.GET)
-//    public List<Category> getAllCategories(Model model) {
-//        List<Category> categoryList = catService.getAllCategories();
-//        model.addAttribute("categoryList", categoryList);
-//        return categoryList;
-//    }
+
+    public String getAllBlogsByCategory(HttpServletRequest request, Model model) {
+        String categoryIdParameter = request.getParameter("categoryId");
+        int categoryId = Integer.parseInt(categoryIdParameter);
+        List<Blog> blogList = service.getAllBlogsByCategory(categoryId);
+        model.addAttribute("blogList", blogList);
+        return "blog";
+    }
+
+    @RequestMapping(value = "getAllCategories", method = RequestMethod.GET)
+    public List<Category> getAllCategories(Model model) {
+        List<Category> categoryList = catService.getAllCategories();
+        model.addAttribute("categoryList", categoryList);
+        return categoryList;
+    }
 }
